5fc5a9c5c3b07ec2f6c7bf1302b6ec11d8df31d5
lulumelhem <32106073+lulumelhem@users.noreply.github.com>
2019-09-30 07:18:42 -0400

    Delete config.js

--- Bootcamp3/server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    db: {
-      uri: 'mongodb+srv://lmelhem:Q1w2e3r4r@cluster0-ofm9b.mongodb.net/test?retryWrites=true&w=majority'
-    }, 
-    openCage: {
-      key: '185e62ffb46540e0ace2f097d61412c8'
-    },
-    port: 8080
-  };
